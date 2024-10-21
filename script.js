c188c657b8a8607dc87fd657c6969a48aafdc3a3
Věra Juračáková <juracakova.vera@gmail.com>
2024-10-21 20:50:37 +0200

    Uložení řešení Michal

--- script.js
+++ script.js
@@ -37,10 +37,47 @@ document.body.innerHTML += "<p>Výměra bytu je: " + apartment.area.floorage + "
 
 document.body.innerHTML += "<p>Byt se nachází ve městě " + city + ", " + district + "</p>"
 
-
-
 document.body.innerHTML += "<p>Stav inzerátu: " + apartment.status + "</p>"
 
 apartment.status = 'taken'
 
 document.body.innerHTML += "<p>Stav inzerátu: " + apartment.status + "</p>"
+
+
+//řešení Michal
+//const apartment = {
+//  type: 'rent',
+//  disposition: '3+1',
+//  area: {
+//    floorage: 100,
+//    balcony: 2,
+//    units: 'sqm',
+//  },
+//  city: 'Prague',
+//  district: 'Old Town',
+//  price: {
+//    rent: 28000,
+//    fees: {
+//      water: 1000,
+//      energy: 2500,
+//      services: 560,
+//    },
+//    currency: 'czk',
+//  },
+//  ownership: 'personal',
+//  condition: 'renovated',
+//  status: 'free',
+//  floor: 3,
+//};
+
+//document.body.innerHTML += "<p>Dispozice: "+ apartment.disposition +"</p>"
+//document.body.innerHTML += "<p>Najem bez poplatku: "+ apartment.price.rent +"</p>"
+//document.body.innerHTML += "<p>Vymera bytu: "+ apartment.area.floorage + apartment.area.units +"</p>"
+
+//const city = apartment.city;
+//const district = apartment.district;
+
+//document.body.innerHTML += "<p>Mesto: "+ city +"</p>"
+//document.body.innerHTML += "<p>Mestska cast: "+ district +"</p>"
+//apartment.status = "taken"
+//document.body.innerHTML += "<p>Stav inzeratu: "+ apartment.status +"</p>"
